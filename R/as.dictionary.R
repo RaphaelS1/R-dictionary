ee373db219fc1f80e3c57bba06c992d9516ffc1e
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-04-11 15:49:48 +0100

    Fixed missing exports

--- R/as.dictionary.R
+++ R/as.dictionary.R
@@ -10,18 +10,23 @@ as.Dictionary.matrix <- function(x,...){
 as.Dictionary.xts <- function(x,...){
   Dictionary$new(x,...)
 }
+#' @export
 as.Dictionary.data.frame <- function(x,...){
   Dictionary$new(x,...)
 }
+#' @export
 as.Dictionary.list <- function(x,...){
   Dictionary$new(x,...)
 }
+#' @export
 as.Dictionary.integer <- function(x,...){
   Dictionary$new(x,...)
 }
+#' @export
 as.Dictionary.numeric <- function(x,...){
   Dictionary$new(x,...)
 }
+#' @export
 as.Dictionary.complex <- function(x,...){
   Dictionary$new(x,...)
 }
