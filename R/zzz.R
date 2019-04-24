29982d627ebac171ba7dfb75c3f760964c7dab1e
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-04-24 10:01:52 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname){
-  print(environment())
-  print(parent.env(environment()))
+  print(get0(Dictionary,envir=parent.env(environemnt())))
   R62S3::R62S3(Dictionary,parent.env(environment()),parent.env(environment()))
   R62S3::R62S3(TypedDictionary,parent.env(environment()),parent.env(environment()))
 }
